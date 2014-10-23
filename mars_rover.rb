2cda031fd814346f38f4ed3579e44f3974e23871
infinitom <deva.nadesan@infinitom.com>
2014-10-23 17:44:33 -0400

    Mars rover LIVES - sorry, no incremental commits because code never worked until the end. When it finally did, it was good!

--- mars_rover.rb
+++ mars_rover.rb
@@ -6,6 +6,7 @@
 # Command - M (move forward in spun direction by one point)
 #
 
+
 class Rover
 	attr_accessor :x, :y, :direction
 
@@ -15,25 +16,82 @@ class Rover
 		@direction = direction
 	end
 
-	def read_instruction
+	def read_instruction(instructions)
+		
+		rover=instructions.split(//)
 
+		rover.each do |command|
+			if command == "M"
+				move 
+			else
+				turn(command)
+			end
+		end
 	end
 
 	def move
-	
+		if @direction == "N"
+			@y += 1
+		elsif @direction == "E"
+			@x += 1
+		elsif @direction == "S"
+			@y -= 1
+		elsif @direction == "W"
+			@x -= 1
+		else
+			puts "Direction unrecognized"
+		end
 	end
 
-	def turn
-	
+	def turn(face)
+		if @direction == "N"
+			if face == "L"
+				@direction = "W"
+			else
+				@direction = "E"
+			end
+		elsif @direction == "E"
+			if face == "L"
+				@direction = "N"
+			else
+				@direction = "S"
+			end
+		elsif @direction == "S"
+			if face == "L"
+				@direction = "E"
+			else
+				@direction = "W"
+			end
+		elsif @direction == "W"
+			if face == "L"
+				@direction = "S"
+			else
+				@direction = "N"
+			end
+		end
 	end	
 
+	def to_s
+		"position is now (#{@x}, #{@y}, \"#{@direction}\")"
+	end
 end
 
 #Create Rovers
-rover1 = Rover.new(0,0,N)
+rover1 = Rover.new(1, 2,"N")
+rover2 = Rover.new(3, 3,"E")
 
 
-# Take Plateau Size
-# puts "Welcome to mars, please specify the plateau size"
-# plateau = gets.chomp
+# Take Rover's instructions
+puts "Welcome to mars"
 
+puts "Please specify Rover 1's moves"
+instructions = gets.chomp
+rover1.read_instruction(instructions)
+
+
+puts "Please specify Rover 2's moves"
+instructions = gets.chomp
+rover2.read_instruction(instructions)
+
+puts "Rover1's new " + rover1.to_s
+puts "Rover2's new " + rover2.to_s
